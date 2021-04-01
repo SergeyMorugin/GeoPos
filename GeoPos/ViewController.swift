33c2f7e2480435bbf784cc91d05b299dc860ebfb
Sergei Morugin <cmorugin@gmail.com>
2021-04-01 18:38:36 +0300

    master

---
+++ GeoPos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeoPos
+//
+//  Created by Matthew on 01.04.2021.
+//  Copyright © 2021 Ostagram Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
