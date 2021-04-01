7884b1cb2f1eec7f933c12c3956f777dd903d72f
Sergei Morugin <cmorugin@gmail.com>
2021-04-01 19:24:45 +0300

    add googlemaps

--- GeoPos/ViewController.swift
+++ GeoPos/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import GoogleMaps
+
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: GMSMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
--- Podfile
+++ Podfile
@@ -7,6 +7,8 @@ target 'GeoPos' do
 
   # Pods for GeoPos
   pod "GoogleMaps"
+
+
   target 'GeoPosTests' do
     inherit! :search_paths
     # Pods for testing
