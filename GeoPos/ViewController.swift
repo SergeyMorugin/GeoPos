aebf0870f7073a77871a08659121037998b90f34
Sergei Morugin <cmorugin@gmail.com>
2021-04-06 18:46:31 +0300

    add scaling for saved route path

--- GeoPos/ViewController.swift
+++ GeoPos/ViewController.swift
@@ -18,18 +18,21 @@ class ViewController: UIViewController {
     var route: GMSPolyline?
     var routePath: GMSMutablePath?
     
+    var dbService: DBService?
     
     @IBOutlet weak var mapView: GMSMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-         configureLocationManager()
+        dbService = RealmService()
+        configureLocationManager()
     }
     
     func initRoutePath(){
         route?.map = nil
         route = GMSPolyline()
+        routePath?.removeAllCoordinates()
         routePath = GMSMutablePath()
         route?.map = mapView
     }
@@ -37,11 +40,12 @@ class ViewController: UIViewController {
     func configureLocationManager() {
         locationManager = CLLocationManager()
         locationManager?.delegate = self
+        //locationManager?.stopUpdatingLocation()
         locationManager?.allowsBackgroundLocationUpdates = true
         locationManager?.pausesLocationUpdatesAutomatically = false
-        locationManager?.startMonitoringSignificantLocationChanges()
         locationManager?.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
         locationManager?.requestAlwaysAuthorization()
+        
     }
     
     func goTo(coordinate: CLLocationCoordinate2D){
@@ -59,15 +63,48 @@ class ViewController: UIViewController {
         route?.path = routePath
     }
     
+    func pathToCoordinates() -> [Coordinate]{
+        guard
+            let path = routePath,
+            let count = routePath?.count()
+        else { return [] }
+        let result = NSMutableArray()
+        for i in 0..<count {
+            let c = path.coordinate(at: i)
+            result.add(Coordinate(latitude: c.latitude, longitude: c.longitude))
+        }
+        return Array(_immutableCocoaArray: result)
+    }
+    
     
     
     @IBAction func startBtnClick(_ sender: Any) {
         initRoutePath()
         locationManager?.startUpdatingLocation()
+        //locationManager?.startMonitoringSignificantLocationChanges()
     }
     
     @IBAction func stopBtnClick(_ sender: Any) {
         locationManager?.stopUpdatingLocation()
+        locationManager?.stopMonitoringSignificantLocationChanges()
+        dbService?.purgeCoordinates()
+        dbService?.save(coordinates: pathToCoordinates())
+    }
+    
+    @IBAction func loadBtnClick(_ sender: Any) {
+        locationManager?.stopUpdatingLocation()
+        locationManager?.stopMonitoringSignificantLocationChanges()
+        let coordinates = dbService?.fetchCoordinates()
+        initRoutePath()
+        coordinates?.forEach({
+            routePath?.add(CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude))
+        })
+        route?.path = routePath
+        let bounds = coordinates?.reduce(GMSCoordinateBounds()) {
+            $0.includingCoordinate(CLLocationCoordinate2D(latitude: $1.latitude, longitude: $1.longitude))
+        }
+
+        mapView.animate(with: .fit(bounds!, withPadding: 30.0))
     }
     
 }
